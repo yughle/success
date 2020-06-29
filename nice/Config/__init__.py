351df61bc9ab4cfc26876083a0b94ae17c7a2d0f
yuzhenyu <895995165@qq.com>
2020-06-29 19:27:22 +0800

    新建

---
+++ nice/Config/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author： yuzhenyu
+# datetime： 2020/6/29 19:21 
+# filename: __init__.py.py
+# development_tool： PyCharm
---
+++ nice/run_main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author： yuzhenyu
+# datetime： 2020/6/29 19:23 
+# filename: run_main.py
+# development_tool： PyCharm
