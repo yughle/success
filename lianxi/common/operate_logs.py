b5f602186c2aa75749c53272c7cbee6fdb54dad0
yuzhenyu <895995165@qq.com>
2020-07-17 17:37:10 +0800

    新建

---
+++ lianxi/common/operate_logs.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author： yuzhenyu
+# datetime： 2020/7/17 13:51 
+# filename: operate_logs.py
+# development_tool： PyCharm
+
+import logging
+
+
+class Log:
+        logger=logging.getLogger()
+
---
+++ lianxi/test_case/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author： yuzhenyu
+# datetime： 2020/7/17 13:48 
+# filename: __init__.py.py
+# development_tool： PyCharm
